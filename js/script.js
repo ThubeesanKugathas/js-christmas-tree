a29e2be9552ffcab785999149248c7a2c14efe96
ThubeesanKugathas <Thubeesan@outlook.com>
2021-12-22 14:38:14 +0100

    pyramid pattern

--- js/script.js
+++ js/script.js
@@ -4,10 +4,33 @@
 function createRow(rowNum) {
 
     let rowContainer = document.querySelector('.container');
-    for (let i = 0; i < rowNum; i++) {
+    for (let i = 1; i <= rowNum; i++) {
         const rowHtml = document.createElement('div');
         rowHtml.className = 'row';
         rowContainer.append(rowHtml);
+
+        // loop cicle per il pyramid pattern: empty boxes di sinistra
+        for (let x = 0; x < (rowNum - i); x++) {
+            const emptyBox1 = document.createElement('div');
+            emptyBox1.className = 'col ms_box';
+            rowHtml.append(emptyBox1);
+        }
+
+        // loop cicle per il pyramid pattern: colored boxes
+        for (let j = 0; j < 2 * i - 1; j++) {
+            const coloredBox = document.createElement('div');
+            coloredBox.className = 'col ms_box colored';
+            rowHtml.append(coloredBox);
+        }
+
+        // loop cicle per il pyramid pattern: empty boxes di destra
+        for (let k = 0; k < (rowNum - i); k++) {
+            const emptyBox2 = document.createElement('div');
+            emptyBox2.className = 'col ms_box';
+            rowHtml.append(emptyBox2);
+        }
+        
+
     }
 }
 
